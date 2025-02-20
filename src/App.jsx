0b69ebc499a982228fc480c56104df73dc92a2a9
Jibalfa <najibalfarizi5@gmail.com>
2025-02-20 21:05:56 +0700

    add environment configuration, API utility, and sign-up schema; implement sign-up functionality in Pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { RouterProvider } from "react-router"
 import router from "./router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App
